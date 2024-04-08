3ff0c36ec101d2831ea290ee09bebbd9b2afef79
Exodus29 <59712509+Exodus29@users.noreply.github.com>
2024-04-08 00:12:57 -0400

    Update main.cpp
    Complete revamp to the game. New things that have been added:
    - Main menu screen 
    - settings screen where you can change the text colour
    - the game itself utilizes OOP for code reusability and simplicity

--- main.cpp
+++ main.cpp
@@ -1,202 +1,271 @@
 #include <iostream>
-
+#include <windows.h>
 using namespace std;
-// void printGrid();
-// void checkWin();
 
-char grid[3][3] = {
-    { 'A', 'B', 'c' }, // row 1
-    { 'D', 'E', 'F' }, // row 2
-    { 'G', 'H', 'I' }  // row 3
-};
-
-void printGrid()
-{
-    cout<<endl;
-    cout<<"    "<<grid[0][0]<<" | "<<grid[0][1]<<" | "<<grid[0][2]<<endl;
-    cout<<"    "<<"----------"<<endl;
-    cout<<"    "<<grid[1][0]<<" | "<<grid[1][1]<<" | "<<grid[1][2]<<endl;
-    cout<<"    "<<"----------"<<endl;
-    cout<<"    "<<grid[2][0]<<" | "<<grid[2][1]<<" | "<<grid[2][2]<<endl;
-    cout<<endl;
-}
-
-
-void checkWin()
-{
-    if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) // check from top right corner to bottom left corner
-    {
-        if(grid[0][0] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[0][0] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-    else if(grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]) // check from top left corner to bottom right
-    {
-        if(grid[0][2] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[0][2] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-    else if(grid[0][0] == grid[0][1] && grid[0][1] == grid[0][2]) // check top row
-    {
-        if(grid[0][0] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[0][0] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-    else if(grid[1][0] == grid[1][1] && grid[1][1] == grid[1][2]) // check middle row
-    {
-        if(grid[1][0] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[1][0] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-    else if(grid[2][0] == grid[2][1] && grid[2][1] == grid[2][2]) // check bottom row
-    {
-        if(grid[2][0] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[2][0] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-    else if(grid[0][0] == grid[1][0] && grid[1][0] == grid[2][0])// check left colum
-    {
-        if(grid[0][0] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[0][0] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-    else if(grid[0][1] == grid[1][1] && grid[1][1] == grid[2][1]) // check middle colum
-    {
-        if(grid[0][1] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[0][1] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-    else if(grid[0][2] == grid[1][2] && grid[1][2] == grid[2][2]) // check right colum
-    {
-        if(grid[0][2] == 'X')
-        {
-            cout<<"Player X wins!"<<endl;
-        }
-        else if(grid[0][2] == 'O')
-        {
-            cout<<"Player O wins!"<<endl;
-        }
-    }
-}
-
+class tictactoe;
+void game();
+void settings();
 
 int main()
 {
-    char entry;
-    cout<<"Tic-Tac-Toe Game"<<endl;
+  int num;
 
-    printGrid();
+  cout<<R"(
+  ___________.__               ___________                     ___________            
+  \__    ___/|__| ____         \__    ___/____    ____         \__    ___/___   ____  
+     |    |   |  |/ ___\   ______ |    |  \__  \ _/ ___\   ______ |    | /  _ \_/ __ \
+     |    |   |  \  \___  /_____/ |    |   / __ \\  \___  /_____/ |    |(  <_> )  ___/ 
+     |____|   |__|\___  >         |____|  (____  /\___  >         |____| \____/ \___  >
+                      \/                       \/     \/                            \/ 
+  )"<<endl;
+  cout<<endl;
 
-    while(true)
+  cout<<"1. Local PvP"<<endl;
+  cout<<"2. Settings"<<endl;
+
+  cout<<"> ";
+  cin>>num;
+
+  if(num == 1)
+  {
+    system("cls");
+    game();
+  }
+  else if(num == 2)
+  {
+    system("cls");
+    settings();
+  }
+  
+  return 0;
+}
+
+
+// settings screen
+void settings()
+{
+  int colourNum;
+
+  cout<<R"(
+        _________       __    __  .__                      
+      /   _____/ _____/  |__/  |_|__| ____    ____  ______
+      \_____  \_/ __ \   __\   __\  |/    \  / ___\/  ___/
+      /        \  ___/|  |  |  | |  |   |  \/ /_/  >___ \ 
+    /_______  /\___  >__|  |__| |__|___|  /\___  /____  >
+            \/     \/                   \//_____/     \/ 
+  )"<<endl;
+  cout<<endl;
+
+  // text colour settings
+  cout<<"Pick a colour setting for the text"<<endl;
+  cout<<endl;
+  cout<<"1. Blue"<<endl;
+  cout<<"2. Green"<<endl;
+  cout<<"3. Aqua"<<endl;
+  cout<<"4. Red"<<endl;
+  cout<<"5. Purple"<<endl;
+  cout<<endl;
+
+  cout<<"> ";
+  cin>>colourNum;
+
+  // set text colour based on input
+  switch (colourNum)
+  {
+  case 1:
+    system("color 01");
+    system("cls");
+    main();
+    break;
+  case 2:
+    system("color 02");
+    system("cls");
+    main();
+    break;
+  case 3:
+    system("color 03");
+    system("cls");
+    main();
+    break;
+  case 4:
+    system("color 04");
+    system("cls");
+    main();
+    break;
+  case 5:
+    system("color 05");
+    system("cls");
+    main();
+    break;
+  default:
+    system("color 0F");
+    system("cls");
+    main();
+    break;
+  }
+}
+
+
+// this class will contain all the information about the game
+class tictactoe
+{
+  private:
+    // 2d array is used to simulate the 2d board in tic tac toe 
+    // letters are used for each position because x and o can't be represented well with numbers
+    char board[3][3] = {
+      {'A', 'B', 'C'},
+      {'D', 'E', 'F'},
+      {'G', 'H', 'I'}
+    };
+
+  public:
+    void printBoard()
     {
-        cout<<"Enter a position to put for x (use capital letters): ";
-        cin>>entry;
-
-        switch(entry)
-        {
-            case 'A':
-                grid[0][0] = 'X';
-                break;
-            case 'B':
-                grid[0][1] = 'X';
-                break;
-            case 'C':
-                grid[0][2] = 'X';
-                break;
-            case 'D':
-                grid[1][0] = 'X';
-                break;
-            case 'E':
-                grid[1][1] = 'X';
-                break;
-            case 'F':
-                grid[1][2] = 'X';
-                break;
-            case 'G':
-                grid[2][0] = 'X';
-                break;
-            case 'H':
-                grid[2][1] = 'X';
-                break;
-            case 'I':
-                grid[2][2] = 'X';
-                break;
-        }
-
-        printGrid();
-        checkWin();
-
-        cout<<"Enter a postion to put for O (use capital letters): ";
-        cin>>entry;
-
-        switch(entry)
-        {
-            case 'A':
-                grid[0][0] = 'O';
-                break;
-            case 'B':
-                grid[0][1] = 'O';
-                break;
-            case 'C':
-                grid[0][2] = 'O';
-                break;
-            case 'D':
-                grid[1][0] = 'O';
-                break;
-            case 'E':
-                grid[1][1] = 'O';
-                break;
-            case 'F':
-                grid[1][2] = 'O';
-                break;
-            case 'G':
-                grid[2][0] = 'O';
-                break;
-            case 'H':
-                grid[2][1] = 'O';
-                break;
-            case 'I':
-                grid[2][2] = 'O';
-                break;
-        }
-
-        printGrid();
-        checkWin();
-
+      cout<<endl;
+      cout<<"    "<<board[0][0]<<" | "<<board[0][1]<<" | "<<board[0][2]<<endl;
+      cout<<"    "<<"----------"<<endl;
+      cout<<"    "<<board[1][0]<<" | "<<board[1][1]<<" | "<<board[1][2]<<endl;
+      cout<<"    "<<"----------"<<endl;
+      cout<<"    "<<board[2][0]<<" | "<<board[2][1]<<" | "<<board[2][2]<<endl;
+      cout<<endl;
     }
+
+    void updateBoard(char input, bool playerXTurn)
+    {
+
+      /*
+      the nested for loop goes through each position on the board and compares 
+      it to the user input. Then when it finds the correct spot it checks which player's 
+      turn it is and places a X or O
+      */
+      for(int i = 0; i < 3; i++)
+      {
+        for(int j = 0; j < 3; j++)
+        {
+          if(board[i][j] == input && playerXTurn == true)
+          {
+            board[i][j] = 'X';
+          } 
+          else if(board[i][j] == input)
+          {
+            board[i][j] = 'O';
+          }
+          else
+          {
+            continue;
+          }
+        }
+      }
+
+      // clear the screen then print the updated board
+      system("cls");
+      printBoard();
+    }
+
+
+    // this function checks for a win by checking every possible win sequence
+    bool checkWin()
+    {
+      if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) // check from top right corner to bottom left corner
+      {
+        return true;
+      }
+      else if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) // check from top left corner to bottom right
+      {
+        return true;
+      }
+      else if(board[0][0] == board[0][1] && board[0][1] == board[0][2]) // check top row
+      {
+        return true;
+      }
+      else if(board[1][0] == board[1][1] && board[1][1] == board[1][2]) // check middle row
+      {
+        return true;
+      }
+      else if(board[2][0] == board[2][1] && board[2][1] == board[2][2]) // check bottom row
+      {
+        return true;
+      }
+      else if(board[0][0] == board[1][0] && board[1][0] == board[2][0])// check left colum
+      {
+        return true;
+      }
+      else if(board[0][1] == board[1][1] && board[1][1] == board[2][1]) // check middle colum
+      {
+        return true;
+      }
+      else if(board[0][2] == board[1][2] && board[1][2] == board[2][2]) // check right colum
+      {
+        return true;
+      }
+
+      return false;
+    }
+
+
+/*
+    void resetBoard()
+    {
+      char board[3][3] = {
+        {'A', 'B', 'C'},
+        {'D', 'E', 'F'},
+        {'G', 'H', 'I'}
+      };
+    }
+*/
+};
+
+
+
+// this function is where the game takes place and where the tictactoe instance will be
+void game()
+{
+  tictactoe game;
+  char input;
+  bool playerXTurn = true;  // player x always goes first so if this is false then it is player O's turn
+
+  game.printBoard();
+
+  while(!game.checkWin()) // loop will continue to run if game.checkWin() returns false
+  {
+    if(playerXTurn == true)
+    {
+      game.checkWin();
+      cout<<"Player X enter a position: ";
+      cin>>input;
+      input = toupper(input);   // convert the input to uppercase to make it easier to find the entered position
+      game.updateBoard(input, playerXTurn);
+      playerXTurn = false;
+    }
+    else
+    {
+      game.checkWin();
+      cout<<"Player O enter a position: ";
+      cin>>input;
+      input = toupper(input);
+      game.updateBoard(input, playerXTurn);
+      playerXTurn = true;
+    }
+  }
+    
+
+  // display which player wins
+  /*
+  The game decides who wins based on the opposite value of what the playerXTurn is currently set to.
+  This means that the previous value is what determines the winner.
+  */
+  if(playerXTurn == false) 
+  {   
+    cout<<"Player X wins!"<<endl;
+    system("pause");
+    system("cls");
+    main();
+  }
+  else
+  {
+    cout<<"Player O wins!"<<endl;
+    system("pause");
+    system("cls");
+    main();
+  }
 }
